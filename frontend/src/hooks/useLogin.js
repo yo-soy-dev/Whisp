9995044a8269cf1d020ac3fcee4377b7207ba9f7
Devansh Tiwari <devanshtiwari817@gmail.com>
2026-01-12 18:58:24 +0530

    Update useLogin.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,11 +1,26 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { login } from "../lib/api";
+// import { useMutation, useQueryClient } from "@tanstack/react-query";
+// import { login } from "../lib/api";
+
+// const useLogin = () => {
+//   const queryClient = useQueryClient();
+//   const { mutate, isPending, error } = useMutation({
+//     mutationFn: login,
+//     onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+//   });
+
+//   return { error, isPending, loginMutation: mutate };
+// };
+
+// export default useLogin;
 
 const useLogin = () => {
   const queryClient = useQueryClient();
+
   const { mutate, isPending, error } = useMutation({
     mutationFn: login,
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+    onSuccess: (data) => {
+      queryClient.setQueryData(["authUser"], data.user);
+    },
   });
 
   return { error, isPending, loginMutation: mutate };
