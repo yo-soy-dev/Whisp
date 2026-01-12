4c37e2debb1989825d511a31132c201f0adb6b84
Devansh Tiwari <devanshtiwari817@gmail.com>
2026-01-12 18:21:41 +0530

    Update OnboardingPage.jsx

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -39,7 +39,7 @@ const OnboardingPage = () => {
 
   const handleRandomAvatar = () => {
     const idx = Math.floor(Math.random() * 100) + 1; // 1-100 included
-    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+    const randomAvatar = `https://api.dicebear.com/7.x/avataaars/svg?seed=${Date.now()}`;
 
     setFormState({ ...formState, profilePic: randomAvatar });
     toast.success("Random profile picture generated!");
