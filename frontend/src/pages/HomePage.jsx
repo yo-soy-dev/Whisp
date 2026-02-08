ca9e4ef74328152ac706649dfb97f5df546c0e54
Devansh Tiwari <devanshtiwari817@gmail.com>
2026-02-08 22:15:11 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -42,7 +42,7 @@ const HomePage = () => {
     const outgoingIds = new Set();
     if (outgoingFriendReqs && outgoingFriendReqs.length > 0) {
       outgoingFriendReqs.forEach((req) => {
-        outgoingIds.add(req.recipient._id);
+        outgoingIds.add(req?.recipient?._id);
       });
       setOutgoingRequestsIds(outgoingIds);
     }
