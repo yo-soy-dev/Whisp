e90f26a6bde011532bc0295254d23a055542380f
Devansh Tiwari <devanshtiwari817@gmail.com>
2026-02-08 21:28:15 +0530

    axios.js Updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-// import path from "path";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -13,12 +13,12 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT;
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "https://whisp-green.vercel.app",
-    credentials: true,
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
   })
 );
 
@@ -29,24 +29,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
-});
-
-app.get("/", (req, res) => {
-  res.json({
-    status: "OK",
-    message: "Whisp backend is running 🚀",
-  });
 });
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,16 @@
+// import axios from "axios";
+
+// const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api";
+// export const axiosInstance = axios.create({
+//   baseURL: BASE_URL,
+//   withCredentials: true, // send cookies with the request
+// });
+
+
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // send cookies with the request
